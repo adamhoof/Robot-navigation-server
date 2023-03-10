aca3d28409aa30274968d1d257316ead1a0b9d0a
adamhoof <adam.hofman1@gmail.com>
2023-03-10 22:04:21 +0100

    oh boi did we just make a whole lot of mess
    Signed-off-by: adamhoof <adam.hofman1@gmail.com>

--- main.go
+++ main.go
@@ -1,20 +1,35 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net"
 	"strconv"
 	"strings"
+	"time"
 )
 
 const (
-	SERVER_ADDRESS = "localhost"
-	SERVER_PORT    = "3999"
-	PROTOCOL       = "tcp"
+	SERVER_ADDRESS         = "localhost"
+	SERVER_PORT            = "3999"
+	PROTOCOL               = "tcp"
+	UNABLE_TO_CLOSE_SOCKET = "unable to close connection\n"
 
 	SERVER_KEY_REQUEST = "107 KEY REQUEST\a\b"
 
-	UNABLE_TO_CLOSE_SOCKET = "unable to close connection\n"
+	SERVER_MOVE = "102 MOVE\a\b"
+
+	SERVER_TURN_LEFT  = "103 TURN LEFT\a\b"
+	SERVER_TURN_RIGHT = "104 TURN RIGHT\a\b"
+	SERVER_PICK_UP    = "105 GET MESSAGE\a\b"
+	SERVER_LOGOUT     = "106 TURN LEFT\a\b"
+
+	SERVER_OK                     = "200 OK\a\b"
+	SERVER_LOGIN_FAILED           = "300 LOGIN FAILED\a\b"
+	SERVER_SYNTAX_ERROR           = "301 SYNTAX ERROR\a\b"
+	SERVER_LOGIC_ERROR            = "302 LOGIC ERROR\a\b"
+	SERVER_KEY_OUT_OF_RANGE_ERROR = "303 KEY OUT OF RANGE\a\b"
 )
 
 type KeyPair struct {
@@ -71,26 +86,30 @@ func FillKeyPairs() (keyPairs []KeyPair) {
 	return keyPairs
 }
 
+func writeToClient(client *Client, message string) error {
+	_, err := fmt.Fprintf(*client.connection, message)
+	return err
+}
+
 func readName(client *Client) (name string, err error) {
-	buffer := make([]byte, 24)
+	buffer := make([]byte, 20)
 	_, err = (*client.connection).Read(buffer)
+	if err != nil {
+		return name, errors.New(SERVER_SYNTAX_ERROR)
+	}
 	name = string(buffer)
 	return name, err
 }
 func checkValidityOfName(name string) (terminatorPosition int, err error) {
 	terminatorPosition = strings.Index(name, "\a\b")
-	if terminatorPosition == -1 {
-		return terminatorPosition, fmt.Errorf("terminator not found")
-	}
-	if terminatorPosition > 20 {
-		return terminatorPosition, fmt.Errorf("invalid length of name")
+	if terminatorPosition == -1 || terminatorPosition > 20 {
+		return terminatorPosition, errors.New(SERVER_SYNTAX_ERROR)
 	}
 	return terminatorPosition, err
 }
 
 func requestKeyID(client *Client) error {
-	_, err := fmt.Fprintf(*client.connection, SERVER_KEY_REQUEST)
-	return err
+	return writeToClient(client, SERVER_KEY_REQUEST)
 }
 
 func readKeyID(client *Client) (keyID int, err error) {
@@ -99,8 +118,9 @@ func readKeyID(client *Client) (keyID int, err error) {
 	stringKeyID := string(buffer)
 	client.KeyIDTerminatorPos = strings.Index(stringKeyID, "\a\b")
 
+	keyID, err = strconv.Atoi(stringKeyID[:client.KeyIDTerminatorPos])
 	//returns int and possibly error
-	return strconv.Atoi(stringKeyID[:client.KeyIDTerminatorPos])
+	return
 }
 
 func countHash(client *Client, keyPairs []KeyPair) int {
@@ -110,14 +130,14 @@ func countHash(client *Client, keyPairs []KeyPair) int {
 	}
 	hash *= 1000
 	hash %= 65536
-	hash += keyPairs[(client.KeyID)-1].ServerKey
+	hash += keyPairs[(client.KeyID)].ServerKey
 	hash %= 65536
 	return hash
 }
 
 func main() {
 	//register key pairs
-	availableKeyPairs := FillKeyPairs()
+	/*availableKeyPairs := FillKeyPairs()*/
 
 	// Create a listener for incoming connections
 	//prevent IPv6 incorrect host input with JoinHostPort()
@@ -139,17 +159,40 @@ func main() {
 	}(listener)
 
 	for {
+		//TODO WRAPPER
+
 		// Wait for a client to connect
-		fmt.Println("Waiting for a connection to connect...")
-		connection, err := listener.Accept()
+		log.Println("Waiting for a client to connect...")
+		conn, err := listener.Accept()
 		if errorOccurred(err, "failed to accept socket communication") {
 			continue
 		}
-		fmt.Printf("Accepted connection from %s\n", connection.RemoteAddr())
-		client := Client{connection: &connection}
+		fmt.Printf("Accepted conn from %s\n", conn.RemoteAddr())
+		/*client := Client{connection: &conn}*/
 
+		err = conn.SetDeadline(time.Now().Add(5 * time.Second))
+		if err != nil {
+			return
+		}
+
+		messageContent := make([]byte, 10000)
+		_, err = conn.Read(messageContent)
+		if err != nil {
+			return
+		}
+
+		cleanedUpMessage := strings.TrimRight(string(messageContent), "\x00")
+		splitMessagesArray := strings.Split(cleanedUpMessage, "\a\b")
+
+		lastIndex := len(splitMessagesArray) - 1
+		if splitMessagesArray[lastIndex] == "" {
+			splitMessagesArray = splitMessagesArray[:lastIndex]
+		}
+		for _, message := range splitMessagesArray {
+			fmt.Printf("Received message: %s\n", message)
+		}
 		//wait for client to send name
-		client.Name, err = readName(&client)
+		/*client.Name, err = readName(scanner)
 		fmt.Println(client.Name)
 		if errorOccurred(err, "failed to read name") {
 			closeSocket(&client)
@@ -159,6 +202,7 @@ func main() {
 		//position return
 		client.NameTerminatorPos, err = checkValidityOfName(client.Name)
 		if errorOccurred(err, "") {
+			_ = writeToClient(&client, err.Error())
 			closeSocket(&client)
 			continue
 		}
@@ -178,6 +222,6 @@ func main() {
 		fmt.Printf("client id: %d\n", client.KeyID)
 
 		hash := countHash(&client, availableKeyPairs)
-		fmt.Printf("hash: %d\n", hash)
+		fmt.Printf("hash: %d\n", hash)*/
 	}
 }
