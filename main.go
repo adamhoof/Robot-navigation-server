8d11417ffd5e8c24a749b06fbd58f47704b59dec
adamhoof <adam.hofman1@gmail.com>
2023-03-30 20:54:50 +0200

    upsie I rewrote the whole thing (kinda)
    Signed-off-by: adamhoof <adam.hofman1@gmail.com>

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"log"
+	"math"
 	"net"
 	"regexp"
 	"strconv"
@@ -20,15 +21,17 @@ const (
 const (
 	TERMINATOR = "\a\b"
 
-	MAX_NAME_LEN  = 18
-	MIN_KEY_INDEX = 0
-	MAX_KEY_INDEX = 4
+	SPACE = ' '
+
+	MAX_NAME_LEN = 18
+	MIN_KEY_ID   = 0
+	MAX_KEY_ID   = 4
 )
 
 type ServerMessage string
 
 const (
-	UNABLE_TO_CLOSE_SOCKET ServerMessage = "unable to cutOff conn\n"
+	UNABLE_TO_CLOSE_SOCKET = "unable to cutOff conn\n"
 
 	SERVER_KEY_REQUEST ServerMessage = "107 KEY REQUEST" + TERMINATOR
 
@@ -37,7 +40,7 @@ const (
 	SERVER_TURN_LEFT  ServerMessage = "103 TURN LEFT" + TERMINATOR
 	SERVER_TURN_RIGHT ServerMessage = "104 TURN RIGHT" + TERMINATOR
 	SERVER_PICK_UP    ServerMessage = "105 GET MESSAGE" + TERMINATOR
-	SERVER_LOGOUT     ServerMessage = "106 TURN LEFT" + TERMINATOR
+	SERVER_LOGOUT     ServerMessage = "106 LOGOUT" + TERMINATOR
 
 	SERVER_OK                     ServerMessage = "200 OK" + TERMINATOR
 	SERVER_LOGIN_FAILED           ServerMessage = "300 LOGIN FAILED" + TERMINATOR
@@ -46,22 +49,36 @@ const (
 	SERVER_KEY_OUT_OF_RANGE_ERROR ServerMessage = "303 KEY OUT OF RANGE" + TERMINATOR
 )
 
-type State int
+type Phase int
 
 const (
-	USERNAME   State = 0
-	KEY        State = 1
-	VALIDATION State = 2
-	MOVE       State = 3
+	CLOSE_CONNECTION Phase = -1
+	USERNAME         Phase = 0
+	KEY              Phase = 1
+	VALIDATION       Phase = 2
+	MOVE             Phase = 3
+	END              Phase = 4
 )
 
-type MoveState int
+type MovePhase int
 
 const (
-	DERIVE_POS MoveState = 0
-	STRAIGHT   MoveState = 1
-	RIGHT      MoveState = 2
-	LEFT       MoveState = 3
+	DERIVE_POS MovePhase = 0
+	STRAIGHT   MovePhase = 1
+	RIGHT      MovePhase = 2
+	LEFT       MovePhase = 3
+)
+
+type Direction int
+
+const (
+	UNKNOWN Direction = -1
+	UP      Direction = 0
+	DOWN    Direction = 1
+	R       Direction = 2
+	L       Direction = 3
+
+	AVOID_OBSTACLE = 4
 )
 
 type KeyPair struct {
@@ -70,11 +87,16 @@ type KeyPair struct {
 }
 
 type Client struct {
-	conn  *net.Conn
-	Name  string
-	KeyID int
-	Hash  int
-	pos   Position
+	conn      *net.Conn
+	Name      string
+	KeyID     int
+	Hash      int
+	phase     Phase
+	movePhase MovePhase
+	pos       Position
+	lastPos   Position
+	dir       Direction
+	numMoves  uint16
 }
 
 type Position struct {
@@ -170,138 +192,294 @@ func createConfirmationCode(clientHash int, key int) (confirmationCode int) {
 func codesMatch(code1 int, code2 int) bool {
 	return code1 == code2
 }
-
-func extractPosition(buffer string, terminator string) (Position, error) {
-	pattern := "OK (-?[0-9]+) (-?[0-9]+)" + terminator
-	matched, _ := regexp.MatchString(pattern, buffer+terminator)
+func extractPosition(message string) (pos Position, err error) {
+	pattern := "^OK (-?[0-9]+) (-?[0-9]+)$"
+	matched, _ := regexp.MatchString(pattern, message)
 	if !matched {
-		return Position{0, 0}, errors.New("wrong coordinates format")
+		return pos, errors.New("wrong coordinates format")
 	}
-	_ = strings.Index(buffer, ",")
-	return Position{
-		x: 0,
-		y: 0,
-	}, nil
+
+	_, err = fmt.Sscanf(message, "OK %d %d", &pos.x, &pos.y)
+	return pos, err
 }
 
-func messageIncomplete(index int) bool {
-	return index == -1
+func isCenterPos(position *Position) bool {
+	return position.x == 0 && position.y == 0
+}
+
+func initialMoveSequenceDone(numSteps uint16) bool {
+	return numSteps >= 2
+}
+
+func unknownDirection(direction Direction) bool {
+	return direction == UNKNOWN
+}
+
+func xAxisCloser(pos *Position) bool {
+	return math.Abs(float64(pos.x)) <= math.Abs(float64(pos.y))
+}
+
+/*func movingAwayFromXAxis(position *Position, lastPosition *Position) bool {
+
+}*/
+
+type MessageType int
+
+const (
+	INCOMPLETE_MESSAGE            MessageType = 1
+	SINGLE_MESSAGE                MessageType = 2
+	MULTI_MESSAGE                 MessageType = 3
+	SINGLE_AND_INCOMPLETE_MESSAGE MessageType = 4
+	MULTI_AND_INCOMPLETE_MESSAGE  MessageType = 5
+	BRUH_MESSAGE                  MessageType = 6
+)
+
+func deriveMessageType(message string, terminator string) MessageType {
+	messageCount := strings.Count(message, terminator)
+	endsWithTerminator := strings.HasSuffix(message, terminator)
+
+	if messageCount == 0 {
+		fmt.Println("incomplete: ", message)
+		return INCOMPLETE_MESSAGE
+
+	} else if messageCount == 1 {
+		if endsWithTerminator {
+			fmt.Println("single: ", message)
+			return SINGLE_MESSAGE
+
+		}
+		fmt.Println("single and incomplete: ", message)
+		return SINGLE_AND_INCOMPLETE_MESSAGE
+
+	} else if messageCount >= 2 {
+		if endsWithTerminator {
+			fmt.Println("multi: ", message)
+			return MULTI_MESSAGE
+		}
+		fmt.Println("multi and incomplete: ", message)
+		return MULTI_AND_INCOMPLETE_MESSAGE
+	}
+	fmt.Println("bruh message: ", message)
+	return BRUH_MESSAGE
+}
+
+func deriveDir(position *Position, lastPosition *Position) Direction {
+	//moved up or down
+	if position.x == lastPosition.x {
+		if position.y == lastPosition.y {
+			return AVOID_OBSTACLE
+		} else if position.y < lastPosition.y {
+			return DOWN
+		} else {
+			return UP
+		}
+		//moved right or left
+	} else if position.y == lastPosition.y {
+		if position.x == lastPosition.x {
+			return AVOID_OBSTACLE
+		} else if position.x < lastPosition.x {
+			return L
+		} else {
+			return R
+		}
+	}
+	//should never happen
+	return UNKNOWN
+}
+
+func handleSingleMessage(singleMessage string, client *Client) (response ServerMessage, nextPhase Phase) {
+	switch client.phase {
+
+	case USERNAME:
+		if len(singleMessage) > MAX_NAME_LEN {
+			return SERVER_SYNTAX_ERROR, CLOSE_CONNECTION
+		}
+		client.setName(singleMessage)
+		return SERVER_KEY_REQUEST, KEY
+
+	case KEY:
+		keyID, err := strconv.Atoi(singleMessage)
+		if err != nil {
+			return SERVER_SYNTAX_ERROR, CLOSE_CONNECTION
+		}
+
+		client.setKeyIndex(keyID)
+		if keyID < MIN_KEY_ID || keyID > MAX_KEY_ID {
+			return SERVER_KEY_OUT_OF_RANGE_ERROR, CLOSE_CONNECTION
+		}
+
+		client.setHash(countHashFromName(client.getName()))
+		serverConfirmationCode := createConfirmationCode(client.getHash(), getKeyPair(client.getKeyIndex()).ServerKey)
+
+		stringHash := strconv.Itoa(serverConfirmationCode) + TERMINATOR
+		return ServerMessage(stringHash), VALIDATION
+
+	case VALIDATION:
+		clientConfirmationCode, err := strconv.Atoi(singleMessage)
+		if err != nil || clientConfirmationCode > 65535 {
+			return SERVER_SYNTAX_ERROR, CLOSE_CONNECTION
+		}
+		confirmationCode := createConfirmationCode(client.getHash(), getKeyPair(client.getKeyIndex()).ClientKey)
+
+		if !codesMatch(confirmationCode, clientConfirmationCode) {
+			return SERVER_LOGIN_FAILED, CLOSE_CONNECTION
+		}
+		sendMessage(client, SERVER_OK)
+
+		return SERVER_MOVE, MOVE
+
+	case MOVE:
+		client.numMoves++
+
+		var err error
+		client.lastPos = client.pos
+		client.pos, err = extractPosition(singleMessage)
+		if err != nil {
+			return SERVER_SYNTAX_ERROR, CLOSE_CONNECTION
+		}
+		if isCenterPos(&client.pos) {
+			return SERVER_PICK_UP, END
+		}
+		if !initialMoveSequenceDone(client.numMoves) {
+			return SERVER_MOVE, MOVE
+		}
+		if unknownDirection(client.dir) {
+			client.dir = deriveDir(&client.pos, &client.lastPos)
+		}
+		if xAxisCloser(&client.pos) {
+			if client.dir == UP || client.dir == DOWN {
+
+			}
+			/*if movingAwayFromXAxis(&client.pos, &client.lastPos) {
+
+			}*/
+		}
+
+	case END:
+		return SERVER_LOGOUT, CLOSE_CONNECTION
+	}
+	return
 }
 
 func handleClient(client *Client) {
-
-	phase := USERNAME
-	movePhase := DERIVE_POS
-
-	dataAsString := ""
-	data := make([]byte, 1024)
+	var buffer = make([]byte, 1024)
+	var message string
 
 	for {
-		// Read data from client
-		n, err := (*client.conn).Read(data)
+		numChars, err := (*client.conn).Read(buffer)
 		if err != nil {
-			//possible more exit point/non-standard situations?
-			log.Println("Error reading data:", err)
+			log.Println("Error reading buffer:", err)
 			cutOff(client)
 			return
+			//possible more exit point/non-standard situations?
 		}
+		err = (*client.conn).SetDeadline(time.Now().Add(1 * time.Second))
 
-		// Append data to dataAsString
-		dataAsString += string(data[:n])
+		message += string(buffer[:numChars])
+		terminatorIndex := strings.Index(message, TERMINATOR)
 
-		// Check if dataAsString contains a complete message
+		messageType := deriveMessageType(message, TERMINATOR)
 
-		terminatorIndex := strings.Index(dataAsString, TERMINATOR)
-		if messageIncomplete(terminatorIndex) {
-			//check if we can early exit the program if error occurs
-			switch phase {
+		switch messageType {
+		case INCOMPLETE_MESSAGE:
+			//check early exit availability
+			switch client.phase {
 			case USERNAME:
-				if len(dataAsString) > MAX_NAME_LEN {
+				if len(message) > MAX_NAME_LEN {
 					sendMessage(client, SERVER_SYNTAX_ERROR)
 
 					cutOff(client)
 					return
 				}
 			case VALIDATION:
-				codeAsNumber, _ := strconv.Atoi(dataAsString)
-				if len(dataAsString) > 5 || codeAsNumber > 65536 {
+				codeAsNumber, _ := strconv.Atoi(message)
+				if len(message) > 5 || codeAsNumber > 65536 {
 					sendMessage(client, SERVER_SYNTAX_ERROR)
 					cutOff(client)
 					return
 				}
+			default:
+				continue
 			}
-			fmt.Println("continuing")
-			continue // Incomplete message in dataAsString, wait for more data
-		}
 
-		// Extract complete message from dataAsString
-		message := dataAsString[:terminatorIndex]
-		dataAsString = dataAsString[terminatorIndex+2:]
+		case SINGLE_MESSAGE:
+			nonTerminatedMessage := message[:terminatorIndex]
 
-		// Process complete message
-		fmt.Printf("Received message: %s\n", message)
+			var response ServerMessage
+			response, client.phase = handleSingleMessage(nonTerminatedMessage, client)
 
-		switch phase {
-		case USERNAME:
-			if len(message) > MAX_NAME_LEN {
-				sendMessage(client, SERVER_SYNTAX_ERROR)
+			if client.phase == CLOSE_CONNECTION {
+				sendMessage(client, response)
 				cutOff(client)
 				return
 			}
-			client.setName(message)
-			sendMessage(client, SERVER_KEY_REQUEST)
-			phase = KEY
-		case KEY:
-			keyID, err := strconv.Atoi(message)
-			if err != nil {
-				sendMessage(client, SERVER_SYNTAX_ERROR)
-				cutOff(client)
-				return
-			}
+			sendMessage(client, response)
+			message = ""
 
-			client.setKeyIndex(keyID)
-			if keyID < MIN_KEY_INDEX || keyID > MAX_KEY_INDEX {
-				sendMessage(client, SERVER_KEY_OUT_OF_RANGE_ERROR)
-				cutOff(client)
-				return
-			}
-
-			client.setHash(countHashFromName(client.getName()))
-			serverConfirmationCode := createConfirmationCode(client.getHash(), getKeyPair(client.getKeyIndex()).ServerKey)
-
-			stringHash := strconv.Itoa(serverConfirmationCode) + "\a\b"
-			sendMessage(client, ServerMessage(stringHash))
-
-			phase = VALIDATION
-		case VALIDATION:
-			clientConfirmationCode, err := strconv.Atoi(message)
-			if err != nil || clientConfirmationCode > 65535 {
-				sendMessage(client, SERVER_SYNTAX_ERROR)
-				cutOff(client)
-				return
-			}
-			validationCode := createConfirmationCode(client.getHash(), getKeyPair(client.getKeyIndex()).ClientKey)
-
-			if !codesMatch(validationCode, clientConfirmationCode) {
-				sendMessage(client, SERVER_LOGIN_FAILED)
-				cutOff(client)
-				return
-			}
-			sendMessage(client, SERVER_OK)
-			phase = MOVE
-			sendMessage(client, SERVER_MOVE)
-		case MOVE:
-			client.pos = Position{x: 0, y: 0}
+		case MULTI_MESSAGE:
 			for {
-				switch movePhase {
-				case STRAIGHT:
+				terminatorIndex = strings.Index(message, TERMINATOR)
+				if terminatorIndex == -1 {
+					break
+				}
+				singleMessage := message[:terminatorIndex]
+				message = message[terminatorIndex+2:]
+
+				switch client.phase {
+				case USERNAME:
+					if len(singleMessage) > MAX_NAME_LEN {
+						sendMessage(client, SERVER_SYNTAX_ERROR)
+						cutOff(client)
+						return
+					}
+					client.setName(singleMessage)
+					sendMessage(client, SERVER_KEY_REQUEST)
+
+					client.phase = KEY
+				case KEY:
+					keyID, err := strconv.Atoi(singleMessage)
+					if err != nil {
+						sendMessage(client, SERVER_SYNTAX_ERROR)
+						cutOff(client)
+						return
+					}
+
+					client.setKeyIndex(keyID)
+					if keyID < MIN_KEY_ID || keyID > MAX_KEY_ID {
+						sendMessage(client, SERVER_KEY_OUT_OF_RANGE_ERROR)
+						cutOff(client)
+						return
+					}
+
+					client.setHash(countHashFromName(client.getName()))
+					serverConfirmationCode := createConfirmationCode(client.getHash(), getKeyPair(client.getKeyIndex()).ServerKey)
+
+					terminatedStringHash := strconv.Itoa(serverConfirmationCode) + TERMINATOR
+					sendMessage(client, ServerMessage(terminatedStringHash))
+
+					client.phase = VALIDATION
+				case VALIDATION:
+					clientConfirmationCode, err := strconv.Atoi(singleMessage)
+					if err != nil || clientConfirmationCode > 65535 {
+						sendMessage(client, SERVER_SYNTAX_ERROR)
+						cutOff(client)
+						return
+					}
+					confirmationCode := createConfirmationCode(client.getHash(), getKeyPair(client.getKeyIndex()).ClientKey)
+
+					if !codesMatch(confirmationCode, clientConfirmationCode) {
+						sendMessage(client, SERVER_LOGIN_FAILED)
+						cutOff(client)
+						return
+					}
+					sendMessage(client, SERVER_OK)
 					sendMessage(client, SERVER_MOVE)
-				case RIGHT:
-					sendMessage(client, SERVER_TURN_RIGHT)
-				case LEFT:
-					sendMessage(client, SERVER_TURN_LEFT)
-				case DERIVE_POS:
-					client.pos, err = extractPosition(message, TERMINATOR)
+
+					client.phase = MOVE
+
+				case MOVE:
+					//even if the client wanted, he can send only one message containing his coordinates within multi message
+					client.pos, err = extractPosition(message)
 					if err != nil {
 						sendMessage(client, SERVER_SYNTAX_ERROR)
 						cutOff(client)
@@ -309,6 +487,16 @@ func handleClient(client *Client) {
 					}
 				}
 			}
+
+		case SINGLE_AND_INCOMPLETE_MESSAGE:
+			break
+			//check if, based on phase, the message makes sense copy from INCOMPLETE_MESSAGE state - ONLY CHECK THE INCOMPLETE MESSAGE, WHETHER IT MAKES SENSE
+		case MULTI_AND_INCOMPLETE_MESSAGE:
+			break
+			//check if, based on phase, the messages makes sense, copy from INCOMPLETE_MESSAGE state - ONLY CHECK THE INCOMPLETE MESSAGE, WHETHER IT MAKES SENSE
+		default:
+			//should not happen
+			return
 		}
 	}
 }
@@ -339,7 +527,7 @@ func main() {
 		if err != nil {
 			return
 		}
-		client := Client{conn: &conn}
+		client := Client{conn: &conn, phase: USERNAME, movePhase: DERIVE_POS, dir: UNKNOWN, numMoves: 0}
 		go handleClient(&client)
 	}
 }
